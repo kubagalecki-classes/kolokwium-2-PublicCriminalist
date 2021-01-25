b6cd29ee1fa500a900b59bd61d29294fe4c440fd
PublicCriminalist <73648683+PublicCriminalist@users.noreply.github.com>
2021-01-25 19:17:35 +0100

    Add files via upload
    Rozw final 1.1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -14,6 +14,7 @@ public:
     Warzywo(const string& n, double c, int k) : nazwa(n), cena(c), kolor(k) { razem++; }
     static unsigned getVeg() { return razem; }
     void opis(ostream& s) const{ s << nazwa << ": " << cena << ", " << kolor<<"\n";}
+    string          getname() { return nazwa; }
     ~Warzywo() { razem--; }
     private:
     string nazwa;
--- include/zad5.hpp
+++ include/zad5.hpp
@@ -4,4 +4,11 @@
 #include <deque>
 #include <vector>
 
-// tutaj algorytm liczKolejneWarzywa
+template<typename T>
+bool liczKolejneWarzywa(T a1, T a2, unsigned n, string& s)
+{
+    int num = count(a1, a2, [](Warzywo w) { return w.getname() == s; });
+    if (num == n)
+        return 1;
+    else return 0;
+}
